2b4273be8e8213c6f63606c97dfc73f7ca0a9026
FARHAD  NAJAFOV <farhadnajafov98.com>
2022-10-15 11:53:59 +0400

    Initial commit

---
+++ src/main/Main.java
@@ -0,0 +1,16 @@
+package main;
+
+import javax.swing.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    JFrame frame=new JFrame("OptionPane");
+    JOptionPane.showMessageDialog(frame,"Hello");
+
+    frame.setSize(400,400);
+    frame.setLayout(null);
+    frame.setVisible(true);
+    }
+}
