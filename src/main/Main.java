94a1a081dea816ab476d81714a7e458d6fbf4649
FARHAD  NAJAFOV <farhadnajafov98.com>
2022-10-15 12:05:57 +0400

    update

--- src/main/Main.java
+++ src/main/Main.java
@@ -7,11 +7,16 @@ public class Main {
     public static void main(String[] args) {
 
     JFrame frame=new JFrame("OptionPane");
-    JOptionPane.showMessageDialog(frame,"Hello");
-    JOptionPane.showMessageDialog(frame,"Message","Warning",JOptionPane.WARNING_MESSAGE);
+    JOptionPane.showMessageDialog(frame,"Message");
+    JOptionPane.showMessageDialog(frame,"Warning","Warning",JOptionPane.WARNING_MESSAGE);
+    JLabel label=new JLabel("Text ====> ");
+    label.setBounds(10,10,1000,20);
 
+    String str=JOptionPane.showInputDialog(frame,"Enter Text");
+    label.setText(label.getText()+str);
     frame.setSize(400,400);
     frame.setLayout(null);
     frame.setVisible(true);
+    frame.add(label);
     }
 }
